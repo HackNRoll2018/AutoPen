0a6fe6aa570fcfac6b64cb71ad62e39265143ffa
Jelena <neohuilingjelena@gmail.com>
2018-01-27 17:17:19 +0800

    working wifi code

--- autopenAR/autopenAR.ino
+++ autopenAR/autopenAR.ino
@@ -1,44 +1,114 @@
-#include <ESP8266WiFi.h> 
-#include <WiFiClientSecure.h> 
-#include <TelegramBot.h> 
-#define LED 1 //led pin number
-// Initialize Wifi connection to the router 
-const char* ssid     = "AutoPen"; 
-const char* password = "autopeniscool"; 
-// Initialize Telegram BOT 
-const char BotToken[] = "541193212:AAFcR1kpHMYdI1MvFjIxdY4Qm-uwrW5t7bw"; 
-WiFiClientSecure net_ssl; 
-TelegramBot bot (BotToken, net_ssl); 
-// the number of the LED pin   
-void setup()  
-{   
-Serial.begin(115200);   
-while (!Serial) {}  //Start running when the serial is open  
-delay(3000);   
-// attempt to connect to Wifi network:   
-Serial.print("Connecting Wifi: ");   
-Serial.println(ssid);   
-while (WiFi.begin(ssid, password) != WL_CONNECTED)  
-      {   
-  Serial.print(".");   
-  delay(500);   
-}   
-Serial.println("");   
-Serial.println("WiFi connected");   
-bot.begin();   
-pinMode(LED, OUTPUT);   
-}   
-void loop()  
-{   
-message m = bot.getUpdates(); // Read new messages   
-if (m.text.equals("on"))  
-      {   
-  digitalWrite(LED, 1);    
-  bot.sendMessage(m.chat_id, "The Led is now ON");   
-}   
-else if (m.text.equals("off"))  
-      {   
-  digitalWrite(LED, 0);    
-  bot.sendMessage(m.chat_id, "The Led is now OFF");   
-}   
-}   
+/*******************************************************************
+    An example of bot that receives commands and turns on and off
+    an LED.
+ *                                                                 *
+    written by Giacarlo Bacchio (Gianbacchio on Github)
+    adapted by Brian Lough
+ *******************************************************************/
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <Servo.h>
+
+// Initialize Wifi connection to the router
+char ssid[] = "AutoPen";     // your network SSID (name)
+char password[] = "autopeniscool"; // your network key
+
+// Initialize Telegram BOT
+#define BOTtoken "541193212:AAFcR1kpHMYdI1MvFjIxdY4Qm-uwrW5t7bw"  // your Bot Token (Get from Botfather)
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+int Bot_mtbs = 1000; //mean time between scan messages
+long Bot_lasttime;   //last time messages' scan has been done
+bool Start = false;
+
+const int ledPin = 13;
+int ledStatus = 0;
+
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i = 0; i < numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    String text = bot.messages[i].text;
+
+    String from_name = bot.messages[i].from_name;
+    if (from_name == "") from_name = "Guest";
+
+    if (text == "/ledon") {
+      digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+      ledStatus = 1;
+      bot.sendMessage(chat_id, "Led is ON", "");
+    }
+
+    if (text == "/ledoff") {
+      ledStatus = 0;
+      digitalWrite(ledPin, LOW);    // turn the LED off (LOW is the voltage level)
+      bot.sendMessage(chat_id, "Led is OFF", "");
+    }
+
+    if (text == "/status") {
+      if (ledStatus) {
+        bot.sendMessage(chat_id, "Led is ON", "");
+      } else {
+        bot.sendMessage(chat_id, "Led is OFF", "");
+      }
+    }
+
+    if (text == "/start") {
+      String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
+      welcome += "This is Flash Led Bot example.\n\n";
+      welcome += "/ledon : to switch the Led ON\n";
+      welcome += "/ledoff : to switch the Led OFF\n";
+      welcome += "/status : Returns current status of LED\n";
+      bot.sendMessage(chat_id, welcome, "Markdown");
+    }
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was Previously
+  // connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  // attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(ledPin, OUTPUT); // initialize digital ledPin as an output.
+  delay(10);
+  digitalWrite(ledPin, LOW); // initialize pin as off
+}
+
+void loop() {
+  if (millis() > Bot_lasttime + Bot_mtbs)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    Bot_lasttime = millis();
+  }
+}
